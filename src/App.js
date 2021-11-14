740df8b253908ada1112beebc6bdb6ae81d87784
Liph-p <wzgl40@163.com>
2021-11-14 18:16:50 +0800

    login page antd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React,{Component} from "react"
+import Login from "./pages/login/login"
+import Admin from "./pages/admin/admin"
+import {Route,Switch} from "react-router-dom"
 export default class App extends Component{
   render(){
     return (
-      <div>APP</div>
+      <div className="app">
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/admin" component={Admin} />
+        </Switch>
+      </div>
     )
   }
 }
